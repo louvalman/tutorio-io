938b681b8f3f1b69dd85d461fadbc2717ce99f48
louvalman <louisdyrhauge@hotmail.com>
2022-02-11 12:42:52 +0100

    animations js for word counter

--- main.js
+++ main.js
@@ -12,3 +12,31 @@ $(window).scroll(function() {
    } 1195.421875
  
 });
+
+/** Counter animation **/
+
+let nCount = function(selector){
+    $(selector).each(function(){
+        $(this).animate(
+            {
+            Counter: $(this).text()
+        },
+        {
+            duration: 4000,
+            easing: "swing",
+            step: function(value){
+                $(this).text(Math.ceil(value));
+            }
+        }
+        );
+    });
+};
+
+let a = 0;
+$(window).scroll(function(){
+    let oTop = $(".numbers").offset().top - window.innerHeight;
+    if (a==0 && $(window).scrollTop() >=oTop){
+        a++;
+        nCount(".rect > h3");
+    }
+});
